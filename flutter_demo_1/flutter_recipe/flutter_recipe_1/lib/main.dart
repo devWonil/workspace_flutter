243b1e0121486c8d966e1fc410a8327c79d7dc37
devWonil <102580743+devWonil@users.noreply.github.com>
2022-07-13 12:35:27 +0900

    플러터SingleTicketProviderStateMixin

--- flutter_demo_1/flutter_recipe/flutter_recipe_1/lib/main.dart
+++ flutter_demo_1/flutter_recipe/flutter_recipe_1/lib/main.dart
@@ -1,24 +1,5 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_recipe_1/pages/recipe_page.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(fontFamily: "PatuaOne"),
-//       home: RecipePage(),
-//     ); //MaterialApp
-//   }
-// }
-
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_1/pages/recipe_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,44 +11,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '위젯연습',
-      home: HomePage(),
-      theme: ThemeData(primarySwatch: Colors.amber),
-    );
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "PatuaOne"),
+      home: RecipePage(),
+    ); //MaterialApp
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+// import 'package:flutter/material.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            MyPicture(),
-            Row(
-              children: [
-                Text("Campground \n fdsfasdfgdsag"),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyPicture extends StatelessWidget {
-  const MyPicture({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Image.asset(
-      "assets/images/articleelevenheader.jpeg",
-    ));
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: '위젯연습',
+//       home: HomePage(),
+//       theme: ThemeData(primarySwatch: Colors.amber),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Scaffold(
+//         backgroundColor: Colors.white,
+//         body: Column(
+//           children: [
+//             MyPicture(),
+//             Row(
+//               children: [
+//                 Text("Campground \n fdsfasdfgdsag"),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class MyPicture extends StatelessWidget {
+//   const MyPicture({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         child: Image.asset(
+//       "assets/images/articleelevenheader.jpeg",
+//     ));
+//   }
+// }
--- flutter_ex_1/lib/main.dart
+++ flutter_ex_1/lib/main.dart
@@ -7,26 +7,100 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // Color color = Theme.of(context).primaryColor;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '위젯연습',
-      home: HomePage(),
-      theme: ThemeData(primarySwatch: Colors.amber),
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Welcome to Flutter"),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection
+          ],
+        ),
+      ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+Widget titleSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: [
+      Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Text(
+                "Oeschinen Lake Campground",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )),
+          Text(
+            'Kandersteg, Switzerland',
+            style: TextStyle(color: Colors.grey[500]),
+          )
+        ],
+      )),
+      Icon(
+        Icons.star,
+        color: Colors.red[500],
+      ),
+      const Text('41'),
+    ],
+  ),
+);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      Scaffold(
-        
-      )
-    );
-  }
+Widget buttonSection = Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+    _buildButtonColumn(Colors.blue, Icons.call, 'CALL'),
+    _buildButtonColumn(Colors.blue, Icons.near_me, 'ROUTE'),
+    _buildButtonColumn(Colors.blue, Icons.share, 'SHARE'),
+  ],
+);
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    children: [
+      Icon(
+        icon,
+        color: color,
+      ),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+              fontSize: 12, fontWeight: FontWeight.w400, color: color),
+        ),
+      ),
+    ],
+  );
 }
+
+Widget textSection = const Padding(
+  padding: EdgeInsets.all(32),
+  child: Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
