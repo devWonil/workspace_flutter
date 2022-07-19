d051e8e783cd93502154019af14a051a8a81b764
devWonil <102580743+devWonil@users.noreply.github.com>
2022-07-19 16:47:48 +0900

    쇼핑카트 완성

---
+++ flutter_shoppingcart_1/lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shopping_cart1/constants.dart';
+
+ThemeData theme() {
+  return ThemeData(
+      primarySwatch: kPrimaryColor, scaffoldBackgroundColor: kPrimaryColor);
+}
